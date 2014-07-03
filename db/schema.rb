3705cc8178f837b1ea721c3fbdcf8198b0b893f3
Sam Bao <sambao21@gmail.com>
2014-07-03 14:59:46 -0500

    Add missing unique email index

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140630192649) do
     t.boolean  "admin",           default: false
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
